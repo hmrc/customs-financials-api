859449b990d725ca7c0804c5514c0f0c80d174cb
DamienButterworth <damienbutterworth@hotmail.co.uk>
2022-04-27 13:01:08 +0100

    MMCA-3449: Added connector for ACC40

---
+++ test/connectors/Acc40ConnectorSpec.scala
@@ -0,0 +1,84 @@
+package connectors
+
+import domain.{AuthoritiesFound, ErrorResponse, NoAuthoritiesFound}
+import domain.acc38.GetCorrespondenceAddressResponse
+import domain.acc40.{Account, CashAccount, DutyDefermentAccount, GeneralGuaranteeAccount, RequestCommon, RequestDetail, ResponseDetail}
+import models.{AccountNumber, EORI}
+import play.api.Application
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+import play.api.test.Helpers._
+import utils.SpecBase
+
+import scala.concurrent.Future
+
+class Acc40ConnectorSpec extends SpecBase {
+
+  "searchAuthorities" should {
+    "return Left no authorities when no authorities returned in the response" in new Setup {
+      when[Future[domain.acc40.Response]](mockHttpClient.POST(any, any, any)(any, any, any, any))
+        .thenReturn(Future.successful(response(None, Some(0), None, None, None)))
+
+      running(app) {
+        val result = await(connector.searchAuthorities(EORI("someEori"), EORI("someEori")))
+        result mustBe Left(NoAuthoritiesFound)
+      }
+    }
+
+    "return Left with error response if the error message present in the response" in new Setup {
+      when[Future[domain.acc40.Response]](mockHttpClient.POST(any, any, any)(any, any, any, any))
+        .thenReturn(Future.successful(response(Some("error message"), Some(0), None, None, None)))
+
+      running(app) {
+        val result = await(connector.searchAuthorities(EORI("someEori"), EORI("someEori")))
+        result mustBe Left(ErrorResponse)
+      }
+    }
+
+    "return Right if a valid response with authorities returned" in new Setup {
+      when[Future[domain.acc40.Response]](mockHttpClient.POST(any, any, any)(any, any, any, any))
+        .thenReturn(Future.successful(response(None, Some(1), Some(Seq(CashAccount(Account("accountNumber", "accountType", "accountOwner"), Some(10.0)))), None, None)))
+
+      running(app) {
+        val result = await(connector.searchAuthorities(EORI("someEori"), EORI("someEori")))
+        result mustBe Right(AuthoritiesFound(Some(1), None, None, Some(Seq(CashAccount(Account("accountNumber", "accountType", "accountOwner"), Some(10.0))))))
+      }
+    }
+  }
+
+
+  trait Setup {
+    implicit val hc: HeaderCarrier = HeaderCarrier()
+    val mockHttpClient: HttpClient = mock[HttpClient]
+
+    def response(error: Option[String],
+                 numberOfAuthorities: Option[Int],
+                 cashAccount: Option[Seq[CashAccount]],
+                 dutyDefermentAccount: Option[Seq[DutyDefermentAccount]],
+                 generalGuaranteeAccount: Option[Seq[GeneralGuaranteeAccount]]
+                ): domain.acc40.Response = domain.acc40.Response(
+      RequestCommon("date", "MDTP", "reference", "CDS"),
+      RequestDetail(EORI("someEORI"), "1", EORI("someOtherEORI")),
+      ResponseDetail(
+        errorMessage = error,
+        numberOfAuthorities = numberOfAuthorities,
+        dutyDefermentAccounts = dutyDefermentAccount,
+        generalGuaranteeAccounts = generalGuaranteeAccount,
+        cdsCashAccounts = cashAccount
+      )
+    )
+
+
+    val app: Application = GuiceApplicationBuilder().overrides(
+      bind[HttpClient].toInstance(mockHttpClient)
+    ).configure(
+      "microservice.metrics.enabled" -> false,
+      "metrics.enabled" -> false,
+      "auditing.enabled" -> false
+    ).build()
+
+    val connector: Acc40Connector = app.injector.instanceOf[Acc40Connector]
+  }
+
+}
