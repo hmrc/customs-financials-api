0a300034ffc0f788a46d9652393c5238b6e0996c
harsha <29434052+harshare@users.noreply.github.com>
2022-08-01 11:18:56 +0100

    MMCA-3497, 3498 Update hmrc-mongo library version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.16.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.56.0"
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.68.0"
   )
 
   val test = Seq(
