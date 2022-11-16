4f04a7d7d596d7f79bd5f734692831baf4a96ec4
Jamal (Abdulmajid) Osman <abdul.majid.ahmed.osman@digital.hmrc.gov.uk>
2022-11-16 15:45:46 +0000

    CDSR-2028: remove unused methods

--- app/domain/tpi02/ResponseDetail.scala
+++ app/domain/tpi02/ResponseDetail.scala
@@ -29,56 +29,6 @@ case class ResponseDetail(CDFPayService: String,
     NDRCCase.flatMap(_.NDRCDetail.declarationID).isDefined ||
       SCTYCase.flatMap(_.declarationID).isDefined
 
-  private def transformedCaseStatus(caseStatus: String): String =
-    caseStatus match {
-      case "Open" => "In Progress"
-      case "Open-Analysis" => "In Progress"
-      case "Pending-Approval" => "In Progress"
-      case "Pending-Queried" => "Pending"
-      case "Resolved-Withdrawn" => "Closed"
-      case "Rejected-Failed Validation" => "Closed"
-      case "Resolved-Rejected" => "Closed"
-      case "Open-Rework" => "In Progress"
-      case "Paused" => "In Progress"
-      case "Resolved-No Reply" => "Closed"
-      case "Resolved-Refused" => "Closed"
-      case "Pending Payment Confirmation" => "In Progress"
-      case "Resolved-Approved" => "Closed"
-      case "Resolved-Partial Refused" => "Closed"
-      case "Pending Decision Letter" => "In Progress"
-      case "Approved" => "In Progress"
-      case "Analysis-Rework" => "In Progress"
-      case "Rework-Payment Details" => "In Progress"
-      case "Pending-RTBH" => "In Progress"
-      case "RTBH Sent" => "Pending"
-      case "Reply To RTBH" => "Pending"
-      case "Pending-Compliance Recommendation" => "In Progress"
-      case "Pending-Compliance Check Query" => "Pending"
-      case "Pending-Compliance Check" => "In Progress"
-    }
-
-  private def transformedCaseStatusScty(caseStatus: String): String =
-    caseStatus match {
-      case "Open" => "In Progress"
-      case "Pending-Approval" =>  "Pending"
-      case "Pending-Payment" =>  "Pending"
-      case "Partial Refund" =>  "Pending"
-      case "Resolved-Refund" =>  "Closed"
-      case "Pending-Query" => "Pending"
-      case "Resolved-Manual BTA" => "Closed"
-      case "Pending-C18" => "Pending"
-      case "Closed-C18 Raised" => "Closed"
-      case "RTBH Letter Initiated" => "Pending"
-      case "Awaiting RTBH Letter Response" => "Pending"
-      case "Reminder Letter Initiated" => "Pending"
-      case "Awaiting Reminder Letter Response" => "Pending"
-      case "Decision Letter Initiated" => "Pending"
-      case "Partial BTA" => "Pending"
-      case "Partial BTA/Refund" => "Pending"
-      case "Resolved-Auto BTA" => "Closed"
-      case "Resolved-Manual BTA/Refund" => "Closed"
-      case "Open-Extension Granted" => "In Progress" //Check these
-    }
 }
 
 object ResponseDetail {
