41e7c859ea328ba6f38c60e2ea0dff126a09fd20
harsha <29434052+harshare@users.noreply.github.com>
2023-05-19 10:19:42 +0100

    MMCA-3614 change EORI validation to use SUB09

--- app/controllers/SubscriptionDisplayRequestController.scala
+++ app/controllers/SubscriptionDisplayRequestController.scala
@@ -16,7 +16,7 @@
 
 package controllers
 
-import connectors.Sub21Connector
+import connectors.Sub09Connector
 import models.EORI
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
 import play.api.{Logger, LoggerLike}
@@ -26,15 +26,15 @@ import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 
-class EORIHistoryRequestController @Inject()(connector: Sub21Connector,
-                                             cc: ControllerComponents)(implicit ec: ExecutionContext) extends BackendController(cc) {
+class SubscriptionDisplayRequestController @Inject()(connector: Sub09Connector,
+                                                     cc: ControllerComponents)(implicit ec: ExecutionContext) extends BackendController(cc) {
 
   val log: LoggerLike = Logger(this.getClass)
 
   def validateEORI(eori: EORI): Action[AnyContent] = Action.async {
-    connector.getEORIHistory(eori).map(eoriHistory => {
-      eoriHistory.getEORIHistoryResponse.responseCommon.status match {
-        case "OK" => Ok
+    connector.getSubscriptions(eori).map(subscriptionDisplay => {
+      subscriptionDisplay.subscriptionDisplayResponse.responseCommon.statusText.isEmpty match {
+        case false => Ok
       }
     }).recover {
       case UpstreamErrorResponse(_, NOT_FOUND, _, _) => NotFound
--- test/controllers/SubscriptionDisplayGetSpecificClaimRequestControllerSpec.scala
+++ test/controllers/SubscriptionDisplayGetSpecificClaimRequestControllerSpec.scala
@@ -16,9 +16,9 @@
 
 package controllers
 
-import connectors.Sub21Connector
+import connectors.Sub09Connector
+import domain.sub09._
 import models.EORI
-import models.responses._
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.mvc.AnyContentAsEmpty
 import play.api.test.FakeRequest
@@ -29,16 +29,18 @@ import utils.SpecBase
 
 import scala.concurrent.Future
 
-class EORIHistoryGetSpecificClaimRequestControllerSpec extends SpecBase {
+class SubscriptionDisplayGetSpecificClaimRequestControllerSpec extends SpecBase {
 
-  "EORIHistoryRequestController.get" should {
+  "SubsciptopRequestController.get" should {
     "validate the EORI and return 200 status code" in new Setup {
-      val responseCommon: EORIHistoryResponseCommon = EORIHistoryResponseCommon("OK", "")
-      val eoriHistory: EORIHistory = EORIHistory(EORI("1212"), Some("1211"), Some("12121"))
-      val eoriHistoryResponseDetail: EORIHistoryResponseDetail = EORIHistoryResponseDetail(Array(eoriHistory))
-      val response: HistoricEoriResponse = HistoricEoriResponse(GetEORIHistoryResponse(responseCommon, eoriHistoryResponseDetail))
+      val responseCommon: ResponseCommon = ResponseCommon("OK", None, "2020-10-05T09:30:47Z", None)
+      val cdsEstablishmentAddress: CdsEstablishmentAddress = CdsEstablishmentAddress("Example Street", "Example", Some("A00 0AA"), "GB")
+      val responseDetail: ResponseDetail = ResponseDetail(Some(EORI("someEori")), None, None, "CDSFullName",
+        cdsEstablishmentAddress, None, None, None,
+        None, None, None, None, None, ETMP_Master_Indicator = true, None)
+      val response: SubscriptionResponse = SubscriptionResponse(SubscriptionDisplayResponse(responseCommon, responseDetail))
 
-      when(mockSub21Connector.getEORIHistory(any))
+      when(mockSub09Connector.getSubscriptions(any))
         .thenReturn(Future.successful(response))
 
       running(app) {
@@ -48,7 +50,7 @@ class EORIHistoryGetSpecificClaimRequestControllerSpec extends SpecBase {
     }
 
     "return 404 for invalid EORI" in new Setup {
-      when(mockSub21Connector.getEORIHistory(any))
+      when(mockSub09Connector.getSubscriptions(any))
         .thenReturn(Future.failed(UpstreamErrorResponse("failed", 404, 404, Map.empty)))
 
       running(app) {
@@ -61,10 +63,10 @@ class EORIHistoryGetSpecificClaimRequestControllerSpec extends SpecBase {
   trait Setup {
     val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest(GET, "/customs-financials-api/eori/testEORI/validate")
     val mockAuthConnector: CustomAuthConnector = mock[CustomAuthConnector]
-    val mockSub21Connector: Sub21Connector = mock[Sub21Connector]
+    val mockSub09Connector: Sub09Connector = mock[Sub09Connector]
 
     val app: Application = GuiceApplicationBuilder().overrides(
-      inject.bind[Sub21Connector].toInstance(mockSub21Connector)
+      inject.bind[Sub09Connector].toInstance(mockSub09Connector)
     ).configure(
       "microservice.metrics.enabled" -> false,
       "metrics.enabled" -> false,
@@ -72,3 +74,4 @@ class EORIHistoryGetSpecificClaimRequestControllerSpec extends SpecBase {
     ).build()
   }
 }
+
