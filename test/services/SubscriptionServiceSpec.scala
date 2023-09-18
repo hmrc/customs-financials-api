9cde27c1afc594171d8df86dc81d21a0def37952
tyagikumarvineet <tyagi.kumar.vineet@gmail.com>
2023-09-18 13:31:54 +0100

    [MMCA-4190] Add tests to achieve the required coverage

--- test/services/SubscriptionServiceSpec.scala
+++ test/services/SubscriptionServiceSpec.scala
@@ -17,8 +17,8 @@
 package services
 
 import connectors.Sub09Connector
-import domain.sub09._
-import models.EORI
+import domain.sub09.{EmailVerifiedResponse, _}
+import models.{EORI, EmailAddress}
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.Helpers.running
 import play.api.{Application, inject}
@@ -33,17 +33,67 @@ class SubscriptionServiceSpec extends SpecBase {
 
     "calling Sub09 get subscriptions" should {
       "get SubscriptionDisplayResponse" in new Setup {
-        val responseCommon: ResponseCommon = ResponseCommon("OK", None, "2020-10-05T09:30:47Z", None)
-        val cdsEstablishmentAddress: CdsEstablishmentAddress = CdsEstablishmentAddress("1 street", "Southampton", Some("SO1 1AA"), "GB")
-        val responseDetail: ResponseDetail = ResponseDetail(Some(eori), None, None, "CDSFullName",
-          cdsEstablishmentAddress, None,
-          None, None, None, None, None, None, None, ETMP_Master_Indicator = true, None)
-        val subscriptionResponse: SubscriptionResponse = SubscriptionResponse(SubscriptionDisplayResponse(responseCommon, responseDetail))
-        when(mockSub09Connector.getSubscriptions(EORI("Trader EORI"))).thenReturn(Future.successful(subscriptionResponse))
+        when(mockSub09Connector.getSubscriptions(EORI("Trader EORI"))).thenReturn(
+          Future.successful(subscriptionResponse))
 
         running(app) {
-          val result = await(service.getVerifiedEmail(EORI("Trader EORI")))
-          result mustBe EmailVerifiedResponse(None)
+          val result = service.getVerifiedEmail(EORI("Trader EORI"))
+          result.map {
+            ev => ev mustBe EmailVerifiedResponse(None)
+          }
+        }
+      }
+    }
+
+    "getEmailAddress" should {
+      "return correct output when there is no contactInformation" in new Setup {
+        when(mockSub09Connector.getSubscriptions(any)).thenReturn(Future.successful(subscriptionResponse))
+
+        running(app) {
+          val result: Future[EmailVerifiedResponse] = service.getEmailAddress(EORI("Trader EORI"))
+
+          result.map {
+            ev => ev mustBe EmailVerifiedResponse(None)
+          }
+        }
+      }
+
+      "return correct output when contactInformation is available" in new Setup {
+        when(mockSub09Connector.getSubscriptions(any)).thenReturn(Future.successful(subscriptionResponseWithContactInfo))
+
+        running(app) {
+          val result: Future[EmailVerifiedResponse] = service.getEmailAddress(EORI("Trader EORI"))
+
+          result.map {
+            ev => ev mustBe EmailVerifiedResponse(Option(EmailAddress(emailAddress)))
+          }
+        }
+      }
+    }
+
+    "getUnverifiedEmail" should {
+      "return correct output when there is no contactInformation" in new Setup {
+        when(mockSub09Connector.getSubscriptions(any)).thenReturn(Future.successful(subscriptionResponse))
+
+        running(app) {
+          val result: Future[EmailUnverifiedResponse] = service.getUnverifiedEmail(EORI("Trader EORI"))
+
+          result.map {
+            eunv => eunv mustBe EmailUnverifiedResponse(None)
+          }
+        }
+      }
+
+      "return correct output when contactInformation is available" in new Setup {
+        when(mockSub09Connector.getSubscriptions(any)).thenReturn(Future.successful(
+          subscriptionResponseWithContactInfo))
+
+        running(app) {
+          val result: Future[EmailUnverifiedResponse] = service.getUnverifiedEmail(EORI("Trader EORI"))
+
+          result.map {
+            eunv => eunv mustBe EmailUnverifiedResponse(Option(EmailAddress(emailAddress)))
+          }
         }
       }
     }
@@ -65,5 +115,26 @@ class SubscriptionServiceSpec extends SpecBase {
     ).build()
 
     val service: SubscriptionService = app.injector.instanceOf[SubscriptionService]
+
+    val responseCommon: ResponseCommon = ResponseCommon("OK", None, "2020-10-05T09:30:47Z", None)
+    val cdsEstablishmentAddress: CdsEstablishmentAddress = CdsEstablishmentAddress(
+      "1 street", "Southampton", Some("SO1 1AA"), "GB")
+
+    val responseDetail: ResponseDetail = ResponseDetail(Some(eori), None, None, "CDSFullName",
+      cdsEstablishmentAddress, None,
+      None, None, None, None, None, None, None, ETMP_Master_Indicator = true, None)
+
+    val emailAddress = "test@gmil.com"
+
+    val contactInfo: ContactInformation = ContactInformation(None, None, None, None, None, None, None, None,
+      emailAddress = Some(EmailAddress(emailAddress)), None)
+
+    val responseDetailWithContactInfo: ResponseDetail = responseDetail.copy(contactInformation = Some(contactInfo))
+
+    val subscriptionResponse: SubscriptionResponse = SubscriptionResponse(
+      SubscriptionDisplayResponse(responseCommon, responseDetail))
+
+    val subscriptionResponseWithContactInfo: SubscriptionResponse = SubscriptionResponse(
+      SubscriptionDisplayResponse(responseCommon, responseDetailWithContactInfo))
   }
 }
