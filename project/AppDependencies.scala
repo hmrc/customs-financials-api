5abd76fc55ae0fc37227f09177b15f706c53f2fb
vineet <vineet.tyagi@digital.hmrc.gov.uk>
2024-05-10 16:01:53 +0100

    [MMCA-4673] Use a val for mongo version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,11 +4,12 @@ import sbt.*
 object AppDependencies {
 
   val bootstrapVersion = "8.6.0"
+  val mongoVersion = "1.9.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % "1.9.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % mongoVersion,
     "com.github.java-json-tools" % "json-schema-validator" % "2.2.14"
   )
 
