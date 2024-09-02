dfa9814196458d2912425e656d8aceb4c0ff3c29
vineet <vineet.tyagi@digital.hmrc.gov.uk>
2024-09-03 00:33:59 +0100

    [MMCA-4874] Update Success response and add tests

--- app/models/responses/CashTransactionsResponse.scala
+++ app/models/responses/CashTransactionsResponse.scala
@@ -17,6 +17,7 @@
 package models.responses
 
 import models.EORI
+import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
 case class CashTransactionsResponse(getCashAccountTransactionListingResponse: GetCashAccountTransactionListingResponse)
@@ -102,6 +103,26 @@ object EoriData {
   implicit val format: OFormat[EoriData] = Json.format[EoriData]
 }
 
+case class EoriDataContainer(eoriData: EoriData)
+
+object EoriDataContainer {
+  implicit val format: OFormat[EoriDataContainer] = Json.format[EoriDataContainer]
+}
+
+case class TaxTypeWithSecurity(reasonForSecurity: Option[String] = None,
+                               taxTypeID: String,
+                               amount: Double)
+
+object TaxTypeWithSecurity {
+  implicit val format: OFormat[TaxTypeWithSecurity] = Json.format[TaxTypeWithSecurity]
+}
+
+case class TaxGroup(taxGroupDescription: String, amount: Double, taxTypes: Seq[TaxTypeWithSecurity])
+
+object TaxGroup {
+  implicit val format: OFormat[TaxGroup] = Json.format[TaxGroup]
+}
+
 case class Declaration(declarationID: String,
                        declarantEORINumber: String,
                        declarantRef: Option[String] = None,
@@ -109,24 +130,11 @@ case class Declaration(declarationID: String,
                        importersEORINumber: String,
                        postingDate: String,
                        acceptanceDate: String,
-                       amount: Double)
+                       amount: Double,
+                       taxGroups: Seq[TaxGroup])
 
 object Declaration {
   implicit val format: OFormat[Declaration] = Json.format[Declaration]
-}
-
-case class TaxGroup(taxGroupDescription: String, amount: Double)
-
-object TaxGroup {
-  implicit val format: OFormat[TaxGroup] = Json.format[TaxGroup]
-}
-
-case class TaxType(reasonForSecurity: Option[String] = None,
-                   taxTypeID: String,
-                   amount: Double)
-
-object TaxType {
-  implicit val format: OFormat[TaxType] = Json.format[TaxType]
 }
 
 object PaymentType extends Enumeration {
@@ -134,10 +142,7 @@ object PaymentType extends Enumeration {
 
   val Payment, Withdrawal, Transfer = Value
 
-  implicit val paymentTypeReads: Reads[PaymentType.Value] = JsPath.read[String].map(strVal => PaymentType.withName(strVal))
-  implicit val paymentTypeWrites: Writes[PaymentType.Value] = Writes { value => JsString(value.toString) }
-
-  implicit val format: Format[PaymentType.Value] = Format(paymentTypeReads, paymentTypeWrites)
+  implicit val format: Format[PaymentType.Value] = Json.formatEnum(PaymentType)
 }
 
 case class PaymentsWithdrawalsAndTransfer(valueDate: String,
@@ -149,25 +154,69 @@ case class PaymentsWithdrawalsAndTransfer(valueDate: String,
                                           sortCode: Option[String] = None)
 
 object PaymentsWithdrawalsAndTransfer {
-  implicit val format: OFormat[PaymentsWithdrawalsAndTransfer] = Json.format[PaymentsWithdrawalsAndTransfer]
+  implicit val paymentWithdrawalsAndTransferReads: Reads[PaymentsWithdrawalsAndTransfer] = (
+    (JsPath \ "valueDate").read[String] and
+      (JsPath \ "postingDate").read[String] and
+      (JsPath \ "paymentReference").read[String] and
+      (JsPath \ "amount").read[Double] and
+      (JsPath \ "type").read[String].map(strVal => PaymentType.withName(strVal)) and
+      (JsPath \ "bankAccount").readNullable[String].map(identity) and
+      (JsPath \ "sortCode").readNullable[String].map(identity)
+    )(PaymentsWithdrawalsAndTransfer.apply _)
+
+  implicit val paymentWithdrawalsAndTransferWrites: Writes[PaymentsWithdrawalsAndTransfer] =
+    (paymentTransfer: PaymentsWithdrawalsAndTransfer) => {
+      Json.obj(
+        "valueDate" -> paymentTransfer.valueDate,
+        "postingDate" -> paymentTransfer.postingDate,
+        "paymentReference" -> paymentTransfer.paymentReference,
+        "amount" -> paymentTransfer.amount,
+        "type" -> paymentTransfer.`type`,
+        "bankAccount" -> paymentTransfer.bankAccount.map(identity),
+        "sortCode" -> paymentTransfer.sortCode.map(identity)
+      )
+    }
+
+  implicit val format: Format[PaymentsWithdrawalsAndTransfer] =
+    Format(paymentWithdrawalsAndTransferReads, paymentWithdrawalsAndTransferWrites)
+}
+
+case class PaymentsWithdrawalsAndTransferContainer(paymentsWithdrawalsAndTransfer: PaymentsWithdrawalsAndTransfer)
+
+object PaymentsWithdrawalsAndTransferContainer {
+  implicit val format: OFormat[PaymentsWithdrawalsAndTransferContainer] = Json.format[PaymentsWithdrawalsAndTransferContainer]
 }
 
 case class CashAccountTransactionSearchResponseDetail(can: String,
-                                                      eoriDetails: Seq[EoriData],
+                                                      eoriDetails: Seq[EoriDataContainer],
                                                       declarations: Option[Seq[Declaration]],
-                                                      taxGroups: Option[Seq[TaxGroup]],
-                                                      taxTypes: Seq[TaxType],
-                                                      paymentsWithdrawalsAndTransfers: Option[Seq[PaymentsWithdrawalsAndTransfer]] = None)
+                                                      paymentsWithdrawalsAndTransfers: Option[Seq[PaymentsWithdrawalsAndTransferContainer]] = None)
 
 object CashAccountTransactionSearchResponseDetail {
-  implicit val format: OFormat[CashAccountTransactionSearchResponseDetail] =
-    Json.format[CashAccountTransactionSearchResponseDetail]
+  implicit val cashAccTransSearchResponseDetailReads: Reads[CashAccountTransactionSearchResponseDetail] = (
+    (JsPath \ "can").read[String] and
+      (JsPath \ "eoriDetails").read[Seq[EoriDataContainer]] and
+      (JsPath \ "declarations").readNullable[Seq[Declaration]].map(identity) and
+      (JsPath \ "paymentsWithdrawalsAndTransfers").readNullable[Seq[PaymentsWithdrawalsAndTransferContainer]].map(identity)
+    )(CashAccountTransactionSearchResponseDetail.apply _)
+
+  implicit val cashAccTransSearchResponseDetailWrites: Writes[CashAccountTransactionSearchResponseDetail] = (
+    (JsPath \ "can").write[String] and
+      (JsPath \ "eoriDetails").write[Seq[EoriDataContainer]] and
+      (JsPath \ "declarations").writeNullable[Seq[Declaration]] and
+      (JsPath \ "paymentsWithdrawalsAndTransfers").writeNullable[Seq[PaymentsWithdrawalsAndTransferContainer]]
+    )(resDetails =>
+    (resDetails.can, resDetails.eoriDetails, resDetails.declarations, resDetails.paymentsWithdrawalsAndTransfers))
+
+  implicit val format: Format[CashAccountTransactionSearchResponseDetail] =
+    Format(cashAccTransSearchResponseDetailReads, cashAccTransSearchResponseDetailWrites)
 }
 
 case class CashAccountTransactionSearchResponse(responseCommon: CashTransactionsResponseCommon,
                                                 responseDetail: Option[CashAccountTransactionSearchResponseDetail] = None)
 
 object CashAccountTransactionSearchResponse {
+  implicit val responseCommonFormat: OFormat[CashTransactionsResponseCommon] = Json.format[CashTransactionsResponseCommon]
   implicit val format: OFormat[CashAccountTransactionSearchResponse] = Json.format[CashAccountTransactionSearchResponse]
 }
 
--- test/models/responses/CashTransactionsResponseSpec.scala
+++ test/models/responses/CashTransactionsResponseSpec.scala
@@ -16,66 +16,265 @@
 
 package models.responses
 
+import models.responses.PaymentType._
+import play.api.libs.json.{JsString, JsSuccess, Json}
 import utils.SpecBase
 
 class CashTransactionsResponseSpec extends SpecBase {
 
+  "PaymentAndWithdrawalDetail" should {
+
+    "generate correct output for Json Writes" in new SetUp {
+      import models.responses.CashTransactionsResponse.paymentAndWithdrawalDetailFormat
+
+      Json.toJson(PaymentAndWithdrawalDetail("999", "test", Some("1234567890987"))) mustBe
+        Json.parse(paymentAndWithdrawalDetailJsString)
+    }
+  }
+
   "PaymentType" should {
 
     "generate correct output for Json Reads" in {
+      val incomingJsValuePayment: JsString = JsString("Payment")
+      val incomingJsValueWithdrawal: JsString = JsString("Withdrawal")
+      val incomingJsValueTransfer: JsString = JsString("Transfer")
 
+      Json.fromJson(incomingJsValuePayment) mustBe JsSuccess(Payment)
+      Json.fromJson(incomingJsValueWithdrawal) mustBe JsSuccess(Withdrawal)
+      Json.fromJson(incomingJsValueTransfer) mustBe JsSuccess(Transfer)
     }
 
     "generate correct output for Json Writes" in {
-
+      Json.toJson(Payment) mustBe JsString("Payment")
+      Json.toJson(Withdrawal) mustBe JsString("Withdrawal")
+      Json.toJson(Transfer) mustBe JsString("Transfer")
     }
   }
 
   "PaymentsWithdrawalsAndTransfer" should {
 
-    "generate correct output for Json Reads" in {
+    "generate correct output for Json Reads" in new SetUp {
+      import models.responses.PaymentsWithdrawalsAndTransfer.format
 
+      Json.fromJson(Json.parse(paymentsWithdrawalAndTransferJsString)) mustBe
+        JsSuccess(paymentsWithdrawalsAndTransferOb)
     }
 
-    "generate correct output for Json Writes" in {
+    "generate correct output for Json Writes" in new SetUp {
+      import models.responses.PaymentsWithdrawalsAndTransfer.format
 
+      Json.toJson(paymentsWithdrawalsAndTransferOb) mustBe Json.parse(paymentsWithdrawalAndTransferJsString)
     }
   }
 
   "CashAccountTransactionSearchResponseDetail" should {
 
-    "generate correct output for Json Reads" in {
+    "generate correct output for Json Reads" in new SetUp {
+      import models.responses.CashAccountTransactionSearchResponseDetail.format
 
+      Json.fromJson(Json.parse(cashAccountTransactionSearchResponseDetailJsString)) mustBe
+        JsSuccess(cashAccountTransactionSearchResponseDetailOb)
     }
 
-    "generate correct output for Json Writes" in {
+    "generate correct output for Json Writes" in new SetUp {
 
+      Json.toJson(cashAccountTransactionSearchResponseDetailOb) mustBe
+        Json.parse(cashAccountTransactionSearchResponseDetailJsString)
     }
   }
 
   "CashAccountTransactionSearchResponse" should {
 
-    "generate correct output for Json Reads" in {
+    "generate correct output for Json Reads" in new SetUp {
+      import models.responses.CashAccountTransactionSearchResponse.format
 
+      Json.fromJson(Json.parse(cashAccountTransactionSearchResponseJsString)) mustBe
+        JsSuccess(cashAccountTransactionSearchResponseOb)
     }
 
-    "generate correct output for Json Writes" in {
-
+    "generate correct output for Json Writes" in new SetUp {
+      Json.toJson(cashAccountTransactionSearchResponseOb) mustBe
+        Json.parse(cashAccountTransactionSearchResponseJsString)
     }
   }
 
   "CashAccountTransactionSearchResponseWrapper" should {
 
-    "generate correct output for Json Reads" in {
+    "generate correct output for Json Reads" in new SetUp {
+      import models.responses.CashAccountTransactionSearchResponseWrapper.format
 
+      Json.fromJson(Json.parse(cashAccTranSearchResponseWrapperJsString)) mustBe
+        JsSuccess(cashAccTranSearchResponseWrapperOb)
     }
 
-    "generate correct output for Json Writes" in {
-
+    "generate correct output for Json Writes" in new SetUp {
+      Json.toJson(cashAccTranSearchResponseWrapperOb) mustBe Json.parse(cashAccTranSearchResponseWrapperJsString)
     }
   }
 
   trait SetUp {
-    val testOb = ""
+    val dateString = "2024-05-28"
+    val processingDate = "2001-12-17T09:30:47Z"
+
+    val paymentReference = "CDSC1234567890"
+    val amount = 9999.99
+    val bankAccount = "1234567890987"
+    val sortCode = "123456789"
+    val can = "12345678909"
+    val eoriNumber = "GB123456789"
+    val eoriDataName = "test"
+
+    val paymentsWithdrawalsAndTransferOb: PaymentsWithdrawalsAndTransfer =
+      PaymentsWithdrawalsAndTransfer(
+        valueDate = dateString,
+        postingDate = dateString,
+        paymentReference = paymentReference,
+        amount = amount,
+        `type` = Payment,
+        bankAccount = Some(bankAccount),
+        sortCode = Some(sortCode)
+      )
+
+    val paymentsWithdrawalAndTransferJsString: String =
+      """{
+        |"valueDate": "2024-05-28",
+        |"postingDate": "2024-05-28",
+        |"paymentReference": "CDSC1234567890",
+        |"amount": 9999.99,
+        |"type": "Payment",
+        |"bankAccount": "1234567890987",
+        |"sortCode": "123456789"
+        |}""".stripMargin
+
+    val paymentAndWithdrawalDetailJsString: String =
+      """{
+        |"amount": "999",
+        |"type": "test",
+        |"bankAccount": "1234567890987"
+        |}""".stripMargin
+
+    val cashAccountTransactionSearchResponseDetailOb: CashAccountTransactionSearchResponseDetail =
+      CashAccountTransactionSearchResponseDetail(
+        can,
+        eoriDetails = Seq(EoriDataContainer(EoriData(eoriNumber, eoriDataName))),
+        declarations = None,
+        paymentsWithdrawalsAndTransfers =
+          Some(
+            Seq(
+              PaymentsWithdrawalsAndTransferContainer(PaymentsWithdrawalsAndTransfer(
+                dateString,
+                dateString,
+                paymentReference,
+                amount,
+                Payment,
+                Some(bankAccount),
+                Some(sortCode)
+              ))
+            ))
+      )
+
+    val cashAccountTransactionSearchResponseDetailJsString: String =
+      """{
+        |"can": "12345678909",
+        |"eoriDetails": [
+        |{
+        |"eoriData": {
+        |"eoriNumber": "GB123456789",
+        |"name": "test"
+        |}
+        |}
+        |],
+        |"paymentsWithdrawalsAndTransfers": [
+        |{
+        |"paymentsWithdrawalsAndTransfer": {
+        |"valueDate": "2024-05-28",
+        |"postingDate": "2024-05-28",
+        |"paymentReference": "CDSC1234567890",
+        |"amount": 9999.99,
+        |"type": "Payment",
+        |"bankAccount": "1234567890987",
+        |"sortCode": "123456789"
+        |}
+        |}
+        |]
+        |}""".stripMargin
+
+    val resCommonOb: CashTransactionsResponseCommon = CashTransactionsResponseCommon(
+      status = "OK",
+      statusText = None,
+      processingDate = processingDate,
+      maxTransactionsExceeded = None)
+
+    val cashAccountTransactionSearchResponseOb: CashAccountTransactionSearchResponse =
+      CashAccountTransactionSearchResponse(resCommonOb, Some(cashAccountTransactionSearchResponseDetailOb))
+
+    val cashAccountTransactionSearchResponseJsString: String =
+      """{
+        |"responseCommon": {
+        |"status": "OK",
+        |"processingDate": "2001-12-17T09:30:47Z"
+        |},
+        |"responseDetail": {
+        |"can": "12345678909",
+        |"eoriDetails": [
+        |{
+        |"eoriData": {
+        |"eoriNumber": "GB123456789",
+        |"name": "test"
+        |}
+        |}
+        |],
+        |"paymentsWithdrawalsAndTransfers": [
+        |{
+        |"paymentsWithdrawalsAndTransfer": {
+        |"valueDate": "2024-05-28",
+        |"postingDate": "2024-05-28",
+        |"paymentReference": "CDSC1234567890",
+        |"amount": 9999.99,
+        |"type": "Payment",
+        |"bankAccount": "1234567890987",
+        |"sortCode": "123456789"
+        |}
+        |}
+        |]
+        |}
+        |}""".stripMargin
+
+    val cashAccTranSearchResponseWrapperOb: CashAccountTransactionSearchResponseWrapper =
+      CashAccountTransactionSearchResponseWrapper(cashAccountTransactionSearchResponseOb)
+
+    val cashAccTranSearchResponseWrapperJsString: String =
+      """{
+        |"cashAccountTransactionSearchResponse": {
+        |"responseCommon": {
+        |"status": "OK",
+        |"processingDate": "2001-12-17T09:30:47Z"
+        |},
+        |"responseDetail": {
+        |"can": "12345678909",
+        |"eoriDetails": [
+        |{
+        |"eoriData": {
+        |"eoriNumber": "GB123456789",
+        |"name": "test"
+        |}
+        |}
+        |],
+        |"paymentsWithdrawalsAndTransfers": [
+        |{
+        |"paymentsWithdrawalsAndTransfer": {
+        |"valueDate": "2024-05-28",
+        |"postingDate": "2024-05-28",
+        |"paymentReference": "CDSC1234567890",
+        |"amount": 9999.99,
+        |"type": "Payment",
+        |"bankAccount": "1234567890987",
+        |"sortCode": "123456789"
+        |}
+        |}
+        |]
+        |}
+        |}
+        |}""".stripMargin
   }
 }
