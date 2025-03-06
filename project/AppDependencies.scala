57adcfcc260d2880002cea0a5011320b02534923
jamie2210 <116884495+jamie2210@users.noreply.github.com>
2025-03-06 16:25:43 +0000

    [MMCA-5178] - Update bootstrap in AppDependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.9.0"
+  val bootstrapVersion = "9.10.0"
   val mongoVersion     = "2.5.0"
 
   val compile: Seq[ModuleID] = Seq(
