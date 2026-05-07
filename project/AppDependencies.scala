c2b81b6d85d471a4281b9c286214b5bdda5fce5d
AdamCoops <adam.cooper2@digital.hmrc.gov.uk>
2026-05-07 10:56:47 +0100

    DCWL-4424-remove 2 vulnerabilities due to low versions in bootstrap/sbtplugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.19.0"
+  val bootstrapVersion = "10.7.0"
   val mongoVersion     = "2.11.0"
 
   val compile: Seq[ModuleID] = Seq(
