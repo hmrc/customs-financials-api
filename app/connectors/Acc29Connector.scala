7264ec682be92baeb6fdc9a0c0c586ccdf84489d
andrew-bintcliffe <186038219+andrew-bintcliffe@users.noreply.github.com>
2026-08-04 10:29:05 +0100

    DCWL-4856 | Add Owner Eori to ACC29

--- app/connectors/Acc29Connector.scala
+++ app/connectors/Acc29Connector.scala
@@ -58,7 +58,21 @@ class Acc29Connector @Inject() (
         .withBody[StandingAuthoritiesRequest](standingAuthoritiesRequest)
         .setHeader(mdgHeaders.headers(appConfig.acc29BearerToken, appConfig.acc29HostHeader): _*)
         .execute[StandingAuthoritiesResponse]
-        .map(_.accounts)
+        .map { response =>
+          val responseWithOwner = response.copy(
+            accounts = response.accounts.map(account =>
+              AccountWithAuthorities(
+                accountType = account.accountType,
+                accountNumber = account.accountNumber,
+                accountStatus = account.accountStatus,
+                ownerEori = Some(response.ownerEori),
+                authorities = account.authorities
+              )
+            )
+          )
+          println("account response test: " + responseWithOwner.accounts)
+          responseWithOwner.accounts
+        }
     }
   }
 }
